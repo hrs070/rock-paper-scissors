ef8cd3e4686ca755799f7592c8c29d0089d30532
hrs070 <harshrajsingh97@gmail.com>
2021-06-11 20:38:15 +0530

    Add 5 rounds before anouncing winner

--- index.js
+++ index.js
@@ -1,18 +1,44 @@
 document.addEventListener("DOMContentLoaded", function () {
     document.getElementById("button").addEventListener("click", function () {
         let result = "";
-        let computerInput = Math.floor((Math.random() * 3) + 1);
-        let userInput = Number(window.prompt("Enter 1 for Rock, 2 for Paper & 3 for Scissors"));
-        if (userInput <= 0 || userInput >= 4) {
-            userInput = Number(window.prompt("Invalid Input!!!\nEnter 1 for Rock, 2 for Paper & 3 for Scissors"));
-        } else if (userInput === computerInput) {
-            result += "It's a Tie !!!";
-        } else if ((userInput === 1 && computerInput === 2) || (userInput === 2 && computerInput === 3) || (userInput === 3 && computerInput === 1)) {
-            result += "You Lose";
-        } else if ((userInput === 1 && computerInput === 3) || (userInput === 2 && computerInput === 1) || (userInput === 3 && computerInput === 2)) {
-            result += "You Win";
+        let userWinCount = 0;
+        let computerWinCount = 0;
+
+        for (let i = 1; i < 6; i++) {
+            setTimeout(function () {
+                result = "";
+                let computerInput = Math.floor((Math.random() * 3) + 1);
+                let userInput = Number(window.prompt("Enter 1 for Rock, 2 for Paper & 3 for Scissors"));
+
+                if (userInput <= 0 || userInput >= 4) {
+                    userInput = Number(window.prompt("Invalid Input!!!\nEnter 1 for Rock, 2 for Paper & 3 for Scissors"));
+                } else if (userInput === computerInput) {
+                    result += "It's a Tie !!!";
+                } else if ((userInput === 1 && computerInput === 2) || (userInput === 2 && computerInput === 3) || (userInput === 3 && computerInput === 1)) {
+                    result += "You Lose";
+                    computerWinCount += 1;
+                } else if ((userInput === 1 && computerInput === 3) || (userInput === 2 && computerInput === 1) || (userInput === 3 && computerInput === 2)) {
+                    result += "You Win";
+                    userWinCount += 1;
+                }
+                document.querySelector("h3").innerText = result;
+                document.getElementById("user-wins").innerText = `User Win Count ${userWinCount}`;
+                document.getElementById("computer-wins").innerText = `Computer Win Count ${computerWinCount}`;
+                console.log([userInput, computerInput, result, i]);
+
+                if (userWinCount > computerWinCount) {
+                    document.querySelector("h2").innerText = `Congratulations! You are the winner.`;
+                } else if (computerWinCount > userWinCount) {
+                    document.querySelector("h2").innerText = `You lost!! Try Again!`;
+                } else {
+                    document.querySelector("h2").innerText = `Good try, but it's a draw!`;
+                }
+
+
+            }, 500)
+
+
         }
-        document.querySelector("h2").innerText = result;
-        console.log([userInput, computerInput, result])
+
     })
 })
